c47107a4bdc456e8ddd808d3c27aa3e1eaacd7ea
Sandeep <sandeep.d@simplilearn.net>
2022-11-18 16:09:43 +0300

    create sign in, sign up, forgot password and OAuth component

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -23,7 +23,7 @@ const [formData, setFormData] = useState({
 
   return (
     <section>
-        <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+        <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
 
         <div className="flex justify-center flex-wrap items-center px-6 py-12   max-w-6xl mx-auto">
 
@@ -81,7 +81,7 @@ const [formData, setFormData] = useState({
                       <Link to="/forgot-password" className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out">Forgot Password?</Link>
                     </p>
                    </div>
-                   <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800" type="submit">Sign Up</button>
+                   <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800" type="submit">Register</button>
 
                  <div className="flex items-center my-4 
                  before:border-t before:flex-1 before:border-gray-300
