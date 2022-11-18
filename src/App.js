ac881bebe640a612a47d55f86aa8fcbcdb7a022b
Sandeep <sandeep.d@simplilearn.net>
2022-11-18 15:57:06 +0300

    create sign in, sign up and forgot password pages and OAuth component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import ForgotPassword from "./pages/ForgotPassword"
-import Offers from "./pages/Offers"
-import Header from "./components/Header"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
 function App() {
   return (
     <>
